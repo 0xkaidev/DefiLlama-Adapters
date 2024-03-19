e17537b1fe54d26232fe13d7ab9a8bc9c5528c55
0xkaidev <0xkai.dev@gmail.com>
2024-03-19 09:31:58 +0700

    v3 on zksync

--- projects/moraswap-v3/index.js
+++ projects/moraswap-v3/index.js
@@ -5,5 +5,10 @@ module.exports = uniV3Export({
     factory: '0x58122246F7e33669cde3486Dd72f95c2e886E375',
     fromBlock: 237396579,
     isAlgebra: false,
+  },
+  era: {
+    factory: '0x62175712623C37FB03281696814E2F96a976376a',
+    fromBlock: 29086333,
+    isAlgebra: false,
   }
 })
